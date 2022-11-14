a366d0f04eb16775b3aa67ee0319ca3c27d391a7
Jarrod Zywien <jarrod.zywien@moodys.com>
2022-11-14 14:46:38 -0500

    added factorial function

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,5 +1,4 @@
 export const sum = (nums: number[]) => nums.reduce((total, curr) => total + curr, 0);
-
 export const sumitorial = (n: number): number => (n === 0 ? 0 : n + sumitorial(n - 1));
-
+export const factorial = (n: number): number => (n === 0 ? 1 : n * factorial(n - 1));
 export const average = (nums: number[]) => Math.round(sum(nums) / nums.length);
